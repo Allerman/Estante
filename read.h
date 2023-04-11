119c5f398843a0a9ed7c46d86da47cbb89a05a82
allerman <allerman3301@gmail.com>
2023-04-10 21:55:02 -0300

    style: organizacao na leitura dos dados

--- read.h
+++ read.h
@@ -1,4 +1,8 @@
 void read(Livro *l) {
-    printf("ID: %d\n", l->id);
-    printf("Name: %s\n", l->name);
+    printf("..............................\n");
+    printf("Seu ultimo registro:\n");
+    printf("\tID: %d\n", l->id);
+    printf("\tBook title: %s\n", l->title);
+    printf("..............................\n");
+
 }
